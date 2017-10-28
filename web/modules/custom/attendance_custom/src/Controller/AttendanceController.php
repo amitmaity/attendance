a7799f65e3817d41eead1a4f0465368a2dba0c12
amitmaity <amit@novatree.com>
2017-10-28 15:38:01 +0530

    Team implemented and logic added accordingly

--- web/modules/custom/attendance_custom/src/Controller/AttendanceController.php
+++ web/modules/custom/attendance_custom/src/Controller/AttendanceController.php
@@ -21,14 +21,21 @@ class AttendanceController extends ControllerBase {
     $object = Node::load($nid);
     $approver_role = \Drupal::currentUser()->getRoles();
     $approver_uid = \Drupal::currentUser()->id();
+    $approver = User::load($approver_uid);
+    $approver_team = $approver->get('field_team')->target_id;
+    $requester_uid = $object->get('field_user')->target_id;
     $requester_uid = $object->field_user->target_id;
     $requester = User::load($requester_uid);
     $requester_role = $requester->getRoles();
+    $requester_team = $requester->get('field_team')->target_id;
     if ($approver_role[1] == 'team_leader' AND $requester_role[1] == 'team_leader'){
       $msg = 'Sorry you are not authorized to approve TL attendance requests';
     }
     elseif ($approver_role[1] == 'employee'){
       $msg = 'Sorry you are not authorized to approve attendance requests';
+    }
+    elseif ($approver_team != $requester_team){
+      $msg = 'Sorry you are not authorized to approve attendance of other team';
     }
     else{
       $object->field_status->value = 'Approved';
--- web/modules/custom/attendance_custom/src/Plugin/Action/ApproveAttendance.php
+++ web/modules/custom/attendance_custom/src/Plugin/Action/ApproveAttendance.php
@@ -24,13 +24,19 @@ class ApproveAttendance extends ActionBase {
   public function execute($object = NULL) {
     $approver_role = \Drupal::currentUser()->getRoles();
     $approver_uid = \Drupal::currentUser()->id();
-    $requester_uid = $object->field_user->target_id;
+    $approver = User::load($approver_uid);
+    $approver_team = $approver->get('field_team')->target_id;
+    $requester_uid = $object->get('field_user')->target_id;
     $requester = User::load($requester_uid);
+    $requester_team = $requester->get('field_team')->target_id;
     $requester_role = $requester->getRoles();
     if ($approver_role[1] == 'team_leader' AND $requester_role[1] == 'team_leader'){
       drupal_set_message('Sorry you are not authorized to approve TL attendance requests');
     }
-    else{
+    elseif ($approver_team != $requester_team){
+      drupal_set_message('Sorry you are not authorized to approve attendance of other team');
+    }
+    else {
       $object->field_status->value = 'Approved';
       $object->field_approved_by->target_id = $approver_uid;
       $object->save();
